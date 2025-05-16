9f0804ec9613f4d0bb82e5258ded6576a86ce650
Diego Hernandez <diego.hdz.cadena@gmail.com>
2025-05-15 18:26:04 -0600

    working sidebar and placeholder pages for alertas and reportes

---
+++ src/pages/alertas.tsx
@@ -0,0 +1,7 @@
+export default function Alertas() {
+  return (
+    <div className="flex h-screen w-full flex-col gap-8 p-12">
+      <h1>Alertas</h1>
+    </div>
+  );
+}
---
+++ src/pages/reportes.tsx
@@ -0,0 +1,7 @@
+export default function Reportes() {
+  return (
+    <div className="flex h-screen w-full flex-col gap-8 p-12">
+      <h1>Reportes</h1>
+    </div>
+  );
+}
