002f6847418fa3d045a6a4da729d8ef7e7cce3ee
Diego Hernandez <diego.hdz.cadena@gmail.com>
2025-05-20 07:27:31 -0600

    Add Observium API configuration and alert fetching functionality
    - Updated .env.example to include Observium API credentials.
    - Updated env.js to validate new environment variables.
    - Created alertRouter to fetch alerts from the Observium API.
    - Introduced AlertSeverity enum for alert status categorization.

---
+++ src/server/api/routers/alert.ts
@@ -0,0 +1,102 @@
+// import { z } from "zod";
+import { env } from "~/env";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import type { AlertSeverity } from "~/utils/types";
+
+export const alertRouter = createTRPCRouter({
+    get: publicProcedure
+    .query(async () => {
+      console.log(env.OBSERVIUM_API_BASE_URL);
+      console.log(env.OBSERVIUM_API_USERNAME);
+  
+      const alerts = await fetch(env.OBSERVIUM_API_BASE_URL + "alerts", {
+        method: "GET",
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `${env.OBSERVIUM_API_USERNAME}:${env.OBSERVIUM_API_PASSWORD}`
+          ).toString("base64")}`,
+          "Content-Type": "application/json",
+        },
+      });
+  
+      console.log("Alerts response:", alerts);
+      if (!alerts.ok) {
+        console.error("Error fetching alerts:", alerts.statusText);
+        throw new Error("Failed to fetch alerts");
+      }
+  
+      // Check if the response is JSON
+      const contentType = alerts.headers.get("content-type");
+      if (!contentType?.includes("application/json")) {
+        console.error("Response is not JSON:", contentType);
+        throw new Error("Response is not JSON");
+      }
+  
+      // Parse the JSON response
+      try {
+        const data = (await alerts.json()) as {
+          status: string;
+          count: string;
+          alerts: Record<string, {
+            alert_table_id: string;
+            alert_test_id: string;
+            device_id: string;
+            entity_type: string;
+            entity_id: string;
+            alert_assocs: string;
+            delay: string;
+            ignore_until: null,
+            ignore_until_ok: string;
+            last_checked: string;
+            last_changed: string;
+            last_recovered: string;
+            last_ok: string;
+            last_failed: string;
+            has_alerted: string;
+            last_message: string;
+            alert_status: string;
+            last_alerted: string;
+            state: string;
+            count: string;
+            severity: string;
+            class: string;
+            html_row_class: string;
+            status: string;
+            checked: string;
+            changed: string;
+            alerted: string;
+            recovered: string;
+            ignore_until_text: string;
+            ignore_until_ok_text: string;
+            humanized: true
+          }>;
+        };
+  
+        console.log("Alerts data:", data);
+  
+        // Transform the alerts object into an array for easier handling
+        const parsedAlerts = Object.values(data.alerts).map((alert) => ({
+          alertTableId: alert.alert_table_id,
+          alertTestId: alert.alert_test_id,
+          deviceId: alert.device_id,
+          entityType: alert.entity_type,
+          entityId: alert.entity_id,
+          alertStatus: alert.alert_status,
+          severity: alert.severity as AlertSeverity | undefined,
+          status: alert.status,
+          lastMessage: alert.last_message,
+          // TODO: Add other fields if needed
+        }));
+  
+        return {
+          status: data.status,
+          count: parseInt(data.count, 10),
+          alerts: parsedAlerts,
+        };
+      } catch (error) {
+        console.error("Error parsing alerts JSON:", error);
+        throw new Error("Error parsing alerts JSON");
+      }
+    }),
+});
