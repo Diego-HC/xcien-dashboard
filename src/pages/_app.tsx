d2f24b38cc7404561af205dde295952bcf533255
Diego Hernandez <diego.hdz.cadena@gmail.com>
2025-05-26 18:03:00 -0600

    format and remove sidebar on login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,26 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import Sidebar from "~/components/Sidebar";
+import { useRouter } from "next/router";
 
 const geist = Geist({
   subsets: ["latin"],
 });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
+  if (currentRoute === "/login") {
+    return (
+      <div
+        className={`${geist.className} flex h-screen w-screen items-center justify-center`}
+      >
+        <Component {...pageProps} />
+      </div>
+    );
+  }
+
   return (
     <div className={`${geist.className} flex flex-row`}>
       <Sidebar />
